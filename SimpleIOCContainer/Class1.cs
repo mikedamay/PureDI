457637af605fb12a94e0aa07e3d0ef94650248b1
mike <mikemay@blueyonder.co.uk>
2017-07-30 07:35:12 +0100

    Add project files.

---
+++ SimpleIOCContainer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIOCContainer
+{
+    public class Class1
+    {
+    }
+}
