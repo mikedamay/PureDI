be50024e86bfcfabc2f144bbf566777519f7057d
mikedamay <mikemay@blueyonder.co.uk>
2017-08-21 09:10:19 +0100

    vscode-git issues fixed

--- SimpleIOCCUser/Program.cs
+++ SimpleIOCCUser/Program.cs
@@ -9,7 +9,6 @@ namespace SimpleIOCCDemo
         static void Main(string[] args)
         {
             SimpleIOCContainer sic = new SimpleIOCContainer();
-            //sic.SetAssemblies(typeof(SimpleIOCContainer).Assembly.GetName().Name);
             TodoProcessor tdp = sic.GetOrCreateObjectTree<TodoProcessor>();
             tdp.Process();
             Thread.Sleep(5000);
