07d07f6282f37fa9d34f43660df44d7961b8400a
mike <mikemay@blueyonder.co.uk>
2017-09-01 20:17:34 +0100

    being careful not tobreak CSharpCompilation.Emit

--- SimpleIOCContainerTest/LoadTests.cs
+++ SimpleIOCContainerTest/LoadTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -17,21 +19,36 @@ namespace IOCCTest.LoadTest
         [TestMethod]
         public void TestLoad()
         {
+            var assembly = BuildAssembly();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            SimpleIOCContainer sic = new SimpleIOCContainer();
+            sic.SetAssemblies(assembly.GetName().Name);
+            object root = sic.CreateAndInjectDependencies("Level1", out var diagnostics);
+            sw.Stop();
+            System.Diagnostics.Debug.WriteLine(sw.Elapsed);
+            System.Diagnostics.Debug.WriteLine(diagnostics);
+            Assert.IsNotNull(root);
+        }
+
+        private Assembly BuildAssembly()
+        {
             var tree = MakeTree();
             //byte[] by = File.ReadAllBytes(@"c:\projects\SimpleIOCContainer\SimpleIOCContainerTest\Test1.cs");
             //string str = new String(Encoding.UTF8.GetChars(by));
             //tree = CSharpSyntaxTree.ParseText(str);
             CSharpCompilation cmp = CSharpCompilation.Create(
-                "LoadTest").AddReferences(new []
+                    "LoadTest").AddReferences(new[]
                 {
-                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
-                    ,MetadataReference.CreateFromFile(typeof(SimpleIOCContainer).Assembly.Location)
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(SimpleIOCContainer).Assembly.Location)
                 })
                 .AddSyntaxTrees(tree).WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
             var ms = new MemoryStream();
             cmp.Emit(ms);
-            Assembly.Load(ms.GetBuffer());
+            Assembly assembly = Assembly.Load(ms.GetBuffer());
             System.Diagnostics.Debug.WriteLine(tree);
+            return assembly;
         }
 
         private SyntaxTree MakeTree()
@@ -46,106 +63,17 @@ namespace IOCCTest.LoadTest
                                 IdentifierName("TheDisappointedProgrammer")),
                             IdentifierName("IOCC")))))
             .WithMembers(
-                List<MemberDeclarationSyntax>(
-                    new MemberDeclarationSyntax[]{
-            //ClassDeclaration("Level1")
-            //.WithAttributeLists(
-            //    SingletonList<AttributeListSyntax>(
-            //        AttributeList(
-            //            SingletonSeparatedList<AttributeSyntax>(
-            //                Attribute(
-            //                    IdentifierName("Bean"))))))
-            //.WithModifiers(
-            //    TokenList(
-            //        Token(SyntaxKind.PublicKeyword)))
-            //.WithMembers(
-            //    List<MemberDeclarationSyntax>(
-            //        new MemberDeclarationSyntax[]{
-            //            FieldDeclaration(
-            //                VariableDeclaration(
-            //                    IdentifierName("Level1a"))
-            //                .WithVariables(
-            //                    SingletonSeparatedList<VariableDeclaratorSyntax>(
-            //                        VariableDeclarator(
-            //                            Identifier("childOne")))))
-            //            .WithAttributeLists(
-            //                SingletonList<AttributeListSyntax>(
-            //                    AttributeList(
-            //                        SingletonSeparatedList<AttributeSyntax>(
-            //                            Attribute(
-            //                                IdentifierName("BeanReference"))))))
-            //            .WithModifiers(
-            //                TokenList(
-            //                    Token(SyntaxKind.PrivateKeyword))),
-            //            FieldDeclaration(
-            //                VariableDeclaration(
-            //                    IdentifierName("Level1b"))
-            //                .WithVariables(
-            //                    SingletonSeparatedList<VariableDeclaratorSyntax>(
-            //                        VariableDeclarator(
-            //                            Identifier("childTwo")))))
-            //            .WithAttributeLists(
-            //                SingletonList<AttributeListSyntax>(
-            //                    AttributeList(
-            //                        SingletonSeparatedList<AttributeSyntax>(
-            //                            Attribute(
-            //                                IdentifierName("BeanReference"))))))
-            //            .WithModifiers(
-            //                TokenList(
-            //                    Token(SyntaxKind.PrivateKeyword)))})),
-            GetClass("Level1", "Level2a", "Level2b")
-            ,GetClass("Level2a","Level2a3a", "Level2a3b")
-            ,GetClass("Level2b","Level2b3a", "Level2b3b")
-                        ,ClassDeclaration("Level2a3a")
-                            .WithAttributeLists(
-                                SingletonList<AttributeListSyntax>(
-                                    AttributeList(
-                                        SingletonSeparatedList<AttributeSyntax>(
-                                            Attribute(
-                                                IdentifierName("Bean"))))))
-                            .WithModifiers(
-                                TokenList(
-                                    Token(SyntaxKind.PublicKeyword))),
-                        ClassDeclaration("Level2a3b")
-                            .WithAttributeLists(
-                                SingletonList<AttributeListSyntax>(
-                                    AttributeList(
-                                        SingletonSeparatedList<AttributeSyntax>(
-                                            Attribute(
-                                                IdentifierName("Bean"))))))
-                            .WithModifiers(
-                                TokenList(
-                                    Token(SyntaxKind.PublicKeyword))),
-                        ClassDeclaration("Level2b3a")
-                            .WithAttributeLists(
-                                SingletonList<AttributeListSyntax>(
-                                    AttributeList(
-                                        SingletonSeparatedList<AttributeSyntax>(
-                                            Attribute(
-                                                IdentifierName("Bean"))))))
-                            .WithModifiers(
-                                TokenList(
-                                    Token(SyntaxKind.PublicKeyword))),
-                        ClassDeclaration("Level2b3b")
-                            .WithAttributeLists(
-                                SingletonList<AttributeListSyntax>(
-                                    AttributeList(
-                                        SingletonSeparatedList<AttributeSyntax>(
-                                            Attribute(
-                                                IdentifierName("Bean"))))))
-                            .WithModifiers(
-                                TokenList(
-                                    Token(SyntaxKind.PublicKeyword)))
+                List<MemberDeclarationSyntax>( MakeClassList().ToArray()
 
-
-                    }))
+                    //}
+        ))
             .NormalizeWhitespace()
             ;
             SyntaxTree tree = CSharpSyntaxTree.Create(cus);
             return tree;
         }
 
-        private ClassDeclarationSyntax GetClass(string className, string childOneName, string ChildTwoName)
+        private ClassDeclarationSyntax MakeClass(string className, string childOneName, string ChildTwoName)
         {
             return ClassDeclaration(className)
                 .WithAttributeLists(
@@ -194,6 +122,36 @@ namespace IOCCTest.LoadTest
                                     TokenList(
                                         Token(SyntaxKind.PrivateKeyword)))
                         }));
+        }
+
+        private ClassDeclarationSyntax MakeLeafClass(string className)
+        {
+            return ClassDeclaration(className)
+                .WithAttributeLists(
+                    SingletonList<AttributeListSyntax>(
+                        AttributeList(
+                            SingletonSeparatedList<AttributeSyntax>(
+                                Attribute(
+                                    IdentifierName("Bean"))))))
+                .WithModifiers(
+                    TokenList(
+                        Token(SyntaxKind.PublicKeyword)));
+        }
+
+        private List<ClassDeclarationSyntax> MakeClassList()
+        {
+            List<ClassDeclarationSyntax> list = new List<ClassDeclarationSyntax>
+            {
+                MakeClass("Level1", "Level2a", "Level2b"),
+                MakeClass("Level2a", "Level2a3a", "Level2a3b"),
+                MakeClass("Level2b", "Level2b3a", "Level2b3b"),
+                MakeLeafClass("Level2a3a"),
+                MakeLeafClass("Level2a3b"),
+                MakeLeafClass("Level2b3a"),
+                MakeLeafClass("Level2b3b")
+            };
+            return list;
+
         }
     }
 }
