596122bc939cf4e75ba0976ea42a55752e22f8d8
mike <mikemay@blueyonder.co.uk>
2018-03-05 09:15:42 +0000

    tweaking docs and namespaces

--- PureDI/DIException.cs
+++ PureDI/DIException.cs
@@ -47,20 +47,4 @@ namespace PureDI
             this.Diagnostics = diagnostics;
         }
    }
-    /// <summary>
-    /// Error indicating some problem with the implementation of the library.
-    /// Some code contract has been violated
-    /// </summary>
-    public class IOCCInternalException : Exception
-    {
-        internal IOCCInternalException(string message) : base(message)
-        {
-        }
-
-        internal IOCCInternalException(string message, Exception innerException)
-          : base(message, innerException)
-        {
-        }
-       
-    }
 }
---
+++ PureDI/Tree/IOCCInternalException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PureDI.Tree
+{
+    /// <summary>
+    /// Error indicating some problem with the implementation of the library.
+    /// Some code contract has been violated
+    /// </summary>
+    internal class IOCCInternalException : Exception
+    {
+        internal IOCCInternalException(string message) : base(message)
+        {
+        }
+
+        internal IOCCInternalException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+       
+    }
+}
